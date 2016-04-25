2d2b659872afa2b4ad223acc87ad3578df64521a
Matti John <matti.john@gmail.com>
2016-04-25 10:53:38 +0100

    Add tests for Service.authorized

---
+++ tests/unit/service/test_authorized.py
@@ -0,0 +1,188 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Open Permissions Platform Coalition
+from __future__ import unicode_literals
+
+import pytest
+import perch
+
+from perch.model import State
+
+client = None
+service = None
+
+# granted access, requested access, result
+examples = [
+    ('r', 'r', True),
+    ('r', 'w', False),
+    ('r', 'rw', False),
+    ('r', '-', False),
+    ('w', 'r', False),
+    ('w', 'w', True),
+    ('w', 'rw', False),
+    ('w', '-', False),
+    ('rw', 'r', True),
+    ('rw', 'w', True),
+    ('rw', 'rw', True),
+    ('rw', '-', False),
+    ('-', 'r', False),
+    ('-', 'w', False),
+    ('-', 'rw', False),
+]
+
+
+def setup():
+    global client
+    global service
+
+    client = perch.Service(**perch.Service.schema({
+        'id': 'client1',
+        'name': 'cilent',
+        'organisation_id': 'client_org',
+        'service_type': 'repository',
+        'state': State.approved.value,
+        'created_by': 'me',
+        'location': 'http://client.test'
+    }))
+    service = perch.Service(**perch.Service.schema({
+        'id': 'service1',
+        'name': 'service',
+        'organisation_id': 'resource_org',
+        'service_type': 'repository',
+        'state': State.approved.value,
+        'created_by': 'me',
+        'location': 'http://service.test',
+    }))
+
+
+def test_authorized_default_service_permissions():
+    assert client.authorized('rw', service)
+
+
+@pytest.mark.parametrize('granted,requested,result', examples)
+def test_service_organisation_access(granted, requested, result):
+    service.permissions = [
+        {
+            'type': 'organisation_id',
+            'permission': granted,
+            'value': client.organisation_id
+        }
+    ]
+
+    assert client.authorized(requested, service) is result
+
+
+@pytest.mark.parametrize('granted,requested,result', examples)
+def test_service_type_access(granted, requested, result):
+    service.permissions = [
+        {
+            'type': 'service_type',
+            'permission': granted,
+            'value': client.service_type
+        }
+    ]
+    assert client.authorized(requested, service) is result
+
+
+@pytest.mark.parametrize('granted,requested,result', examples)
+def test_service_all_access(granted, requested, result):
+    service.permissions = [
+        {
+            'type': 'all',
+            'permission': granted,
+            'value': None
+        }
+    ]
+
+    assert client.authorized(requested, service) is result
+
+
+@pytest.mark.parametrize('state', [x for x in State if x != State.approved])
+def test_client_not_approved(state):
+    client.state = state.value
+    assert not client.authorized('r', service)
+
+
+@pytest.mark.parametrize('state', [x for x in State if x != State.approved])
+def test_service_not_approved(state):
+    service.state = state.value
+    assert not client.authorized('r', service)
+
+
+def test_organisation_access_priority():
+    service.permissions = [
+        {
+            'type': 'all',
+            'permission': '-',
+            'value': None
+        },
+        {
+            'type': 'service_type',
+            'permission': '-',
+            'value': client.service_type
+        },
+        {
+            'type': 'organisation_id',
+            'permission': 'w',
+            'value': client.organisation_id
+        }
+    ]
+
+    assert client.authorized('w', service)
+
+
+def test_service_type_priority():
+    service.permissions = [
+        {
+            'type': 'all',
+            'permission': '-',
+            'value': None
+        },
+        {
+            'type': 'service_type',
+            'permission': 'w',
+            'value': client.service_type
+        }
+    ]
+
+    assert client.authorized('w', service)
+
+
+def test_multiple_rules():
+    service.permissions = [
+        {
+            'type': 'organisation_id',
+            'permission': 'r',
+            'value': client.organisation_id
+        },
+        {
+            'type': 'organisation_id',
+            'permission': 'w',
+            'value': client.organisation_id
+        }
+    ]
+
+    assert client.authorized('w', service)
+    assert client.authorized('r', service)
+    assert client.authorized('rw', service)
+
+
+def test_multiple_rules_denied():
+    service.permissions = [
+        {
+            'type': 'organisation_id',
+            'permission': 'r',
+            'value': client.organisation_id
+        },
+        {
+            'type': 'organisation_id',
+            'permission': '-',
+            'value': client.organisation_id
+        },
+        {
+            'type': 'organisation_id',
+            'permission': 'w',
+            'value': client.organisation_id
+        }
+    ]
+
+    assert not client.authorized('w', service)
