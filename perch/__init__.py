708afb502c9823d5e2f9d518b774878b01a6b5a2
cliff xuan <cliff.xuan@gmail.com>
2016-04-29 13:30:40 +0000

    use cloverpy from pypi

--- perch/__init__.py
+++ perch/__init__.py
@@ -12,7 +12,7 @@ from .model import *
 from .user import *
 from .organisation import *
 
-__version__ = '0.15.1'
+__version__ = '0.15.2'
 
 define('db_port', default=5984)
 define('url_registry_db', default='http://localhost')
