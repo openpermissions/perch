c10ddf1219e39b282f5fd13ab0045d3a926a77c3
Tessa Conway <tessa.conway@cde.catapult.org.uk>
2016-08-18 11:09:57 +0100

    0.15.14

--- perch/__init__.py
+++ perch/__init__.py
@@ -13,7 +13,7 @@ from .user import *
 from .organisation import *
 from . import migrate
 
-__version__ = '0.15.13'
+__version__ = '0.15.14'
 
 define('db_port', default=5984)
 define('url_registry_db', default='http://localhost')
